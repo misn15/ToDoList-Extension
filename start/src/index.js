79c5697505543cc12314f2b4265616e1c1818074
Nicole Mis <misn15@bu.edu>
2021-06-01 01:32:04 -0400

    adding functionality to view and create items

--- start/src/index.js
+++ start/src/index.js
@@ -2,7 +2,10 @@ import axios from '../node_modules/axios';
 
 // form fields
 const form = document.querySelector('.form-data');
+const task = document.querySelector('.task-form');
+const taskData = document.querySelector('.task-data');
 const apiKey = document.querySelector('.api-key');
+let item = null;
 
 // results
 const errors = document.querySelector('.errors');
@@ -12,15 +15,14 @@ const projects = document.querySelector('.projects');
 const clearBtn = document.querySelector('.clear-btn');
 
 form.addEventListener('submit', (e) => handleSubmit(e));
-clearBtn.addEventListener('click', (e) => reset(e));
+task.addEventListener('submit', (e) => register(e));
+clearBtn.addEventListener('click', (e) => displayTaskForm(e));
 init();
 
 function init() {
 	//if anything is in localStorage, pick it up
 	const storedApiKey = localStorage.getItem('apiKey');
-
-	//set icon to be generic green
-	//todo
+	task.style.display = 'none';
 
 	if (storedApiKey === null) {
 		//if we don't have the keys, show the form
@@ -28,22 +30,30 @@ function init() {
 		results.style.display = 'none';
 		loading.style.display = 'none';
 		clearBtn.style.display = 'none';
-		errors.textContent = '';
+		task.style.display = 'none';
 	} else {
         //if we have saved keys/regions in localStorage, show results when they load
-        displayProjects(storedApiKey);
+        displayTasks(storedApiKey);
 		results.style.display = 'none';
 		form.style.display = 'none';
 		clearBtn.style.display = 'block';
+		task.style.display = 'none';
 	}
 };
 
-function reset(e) {
+// function reset(e) {
+// 	e.preventDefault();
+// 	//clear local storage for region only
+// 	localStorage.removeItem('apiKey');
+// 	//localStorage.removeItem('regionName');
+// 	init();
+// }
+
+function displayTaskForm(e) {
 	e.preventDefault();
-	//clear local storage for region only
-	localStorage.removeItem('apiKey');
-	//localStorage.removeItem('regionName');
-	init();
+	task.style.display = 'block';
+	results.style.display = 'none';
+	clearBtn.style.display = 'none';
 }
 
 function handleSubmit(e) {
@@ -57,10 +67,10 @@ function setUpUser(apiKey) {
 	errors.textContent = '';
 	clearBtn.style.display = 'block';
 	//make initial call
-	displayProjects(apiKey);
+	displayTasks(apiKey);
 }
 
-async function displayProjects(apiKey) {
+async function displayTasks(apiKey) {
 	try {
 		await axios
 			.get('https://api.todoist.com/rest/v1/tasks', {
@@ -71,13 +81,14 @@ async function displayProjects(apiKey) {
 				},
 			})
 			.then((response) => {
-				//let projects = Math.floor(response.data.data.carbonIntensity);
-				console.log(response.data);
-
 
 				loading.style.display = 'none';
 				form.style.display = 'none';
-				projects.textContent = 'success';
+				response.data.map(item => {
+					let node = document.createElement("li"); 
+					node.appendChild(document.createTextNode(item.content));
+					projects.appendChild(node);
+				});
 				results.style.display = 'block';
 			});
 	} catch (error) {
@@ -87,3 +98,31 @@ async function displayProjects(apiKey) {
 		errors.textContent = 'Sorry, we have no data for the region you have requested.';
 	}
 }
+
+async function register(e) {
+	e.preventDefault();
+
+	const formData = new FormData(task);
+	const data = JSON.stringify(Object.fromEntries(formData));
+	//taskData.textContent="task added";
+	task.style.display = "none";
+	taskData.style.display = 'block';
+	const result = await createTask(data, apiKey.value);
+
+	item = result;
+  }
+
+async function createTask(item, apiKey) {
+	try {
+	  const response = await fetch('https://api.todoist.com/rest/v1/tasks', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' , 'Authorization': "Bearer "+ apiKey},
+		body: item
+		
+	  });
+	  return await response.json();
+	} catch (error) {
+	  return { error: error.message || 'Unknown error' };
+	}
+	
+  }
