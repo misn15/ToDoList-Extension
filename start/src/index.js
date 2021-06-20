e1d85ac01149e0ea881cebd2bc134fb4c7d28830
Nicole Mis <misn15@bu.edu>
2021-06-20 16:30:16 -0400

    fixing delete and add to do item functionality

--- start/src/index.js
+++ start/src/index.js
@@ -5,50 +5,43 @@ const form = document.querySelector('.form-data');
 const task = document.querySelector('.task-form');
 const taskData = document.querySelector('.task-data');
 const apiKey = document.querySelector('.api-key');
+const apiInput = document.querySelector('.api-task');
 const buttons = document.querySelector('.buttons');
 const backBtn = document.querySelector('.back-btn');
 const projects = document.getElementById('projects');
+const clearBtn = document.querySelector('.clear-btn');
 let item = null;
 
 // results
 const errors = document.querySelector('.errors');
 const loading = document.querySelector('.loading');
 const results = document.querySelector('.result-container');
-const clearBtn = document.querySelector('.clear-btn');
-const deleteBtn = document.querySelector('.delete-btn');
 
 form.addEventListener('submit', (e) => handleSubmit(e));
 task.addEventListener('submit', (e) => register(e));
 clearBtn.addEventListener('click', (e) => displayTaskForm(e));
-backBtn.addEventListener('click', () => {
-	results.style.display = 'none';
-	taskData.style.display = 'none';
-	buttons.style.display = 'block';
-	displayTasks(apiKey.value)
-});
+backBtn.addEventListener('click', (e) => goBack(e));
+
+
 init();
 
 function init() {
 	//if anything is in localStorage, pick it up
 	const storedApiKey = localStorage.getItem('apiKey');
+	
 	task.style.display = 'none';
 	taskData.style.display = 'none';
-	results.style.display = 'none';
-	
+	results.style.display = 'none';	
 
 	if (storedApiKey === null) {
 		//if we don't have the keys, show the form
 		form.style.display = 'block';
 		loading.style.display = 'none';
-		// clearBtn.style.display = 'none';
-		// deleteBtn.style.display = 'none';
 		buttons.style.display = 'none';
 	} else {
         //if we have saved keys/regions in localStorage, show results when they load
         displayTasks(storedApiKey);
 		form.style.display = 'none';
-		// clearBtn.style.display = 'block';
-		// deleteBtn.style.display = 'block';
 		buttons.style.display = 'block';
 	}
 };
@@ -65,8 +58,6 @@ function displayTaskForm(e) {
 	e.preventDefault();
 	task.style.display = 'block';
 	results.style.display = 'none';
-	// clearBtn.style.display = 'none';
-	// deleteBtn.style.display = 'none';
 	buttons.style.display = 'none';
 }
 
@@ -79,65 +70,74 @@ function setUpUser(apiKey) {
 	localStorage.setItem('apiKey', apiKey);
 	loading.style.display = 'block';
 	errors.textContent = '';
-	// clearBtn.style.display = 'block';
-	// deleteBtn.style.display = 'block';
 	buttons.style.display = 'block';
 	//make initial call
 	displayTasks(apiKey);
 }
 
 async function displayTasks(apiKey) {
+	let apiCode = apiKey;
+	if (apiKey === '') {
+		apiCode = apiInput.value;	
+	} else {
+		apiCode = apiKey;
+	}
 	try {
 		await axios
 			.get('https://api.todoist.com/rest/v1/tasks', {
 				params: {
 				},
 				headers: {
-					'Authorization': "Bearer "+ apiKey
+					'Authorization': "Bearer "+ apiCode
 				},
 			})
-			.then((response) => {
+			.then((response) => {				
 				projects.innerHTML = '';
 				loading.style.display = 'none';
 				form.style.display = 'none';
-				
 				response.data.map(item => {
 					let checkbox = document.createElement("input");
 					let br = document.createElement("br");
 					checkbox.setAttribute("type", "checkbox");
-					checkbox.id = "id";
+					checkbox.id = item.id;
+					checkbox.name = "task";
+					checkbox.value = item.id;
 					let label = document.createElement('label');
-					label.htmlFor = "id";
-					label.appendChild(document.createTextNode(item.content));
-					//node.appendChild(document.createTextNode(item.content));
-
-					//node.appendChild(document.createTextNode(item.content));
-					
+					label.htmlFor = item.id;
+					label.appendChild(document.createTextNode(item.content));	
+					checkbox.addEventListener("click", function() {label.style.setProperty('text-decoration', 'line-through');	
+					completeTask(checkbox.value, apiKey)});			
 					projects.appendChild(checkbox);
 					projects.appendChild(label);
 					projects.appendChild(br);
 	
 				});
 				results.style.display = 'block';
-			});
-	} catch (error) {
-		console.log(error);
+			});	
+	} 
+	 catch (error) {		
 		loading.style.display = 'none';
 		results.style.display = 'none';
 		errors.textContent = 'Sorry, we have no data for the region you have requested.';
 	}
 }
 
+function goBack(e) {
+	e.preventDefault();
+	results.style.display = 'none';
+	taskData.style.display = 'none';
+	buttons.style.display = 'block';
+	displayTasks(apiKey.value);
+}
+
+
 async function register(e) {
 	e.preventDefault();
 
 	const formData = new FormData(task);
 	const data = JSON.stringify(Object.fromEntries(formData));
-	//taskData.textContent="task added";
 	task.style.display = "none";
-	//taskData.style.display = 'block';
-	
-	
+		
 	const result = await createTask(data, apiKey.value);
 	taskData.style.display = 'block';
 	
@@ -145,16 +145,52 @@ async function register(e) {
   }
 
 async function createTask(item, apiKey) {
+	let apiCode = apiKey;
+	if (apiKey === '') {
+		apiCode = apiInput.value;	
+	} else {
+		apiCode = apiKey;
+	}
 	try {
 	  const response = await fetch('https://api.todoist.com/rest/v1/tasks', {
 		method: 'POST',
-		headers: { 'Content-Type': 'application/json' , 'Authorization': "Bearer "+ apiKey},
-		body: item
-		
+		headers: { 'Content-Type': 'application/json' , 'Authorization': "Bearer "+ apiCode},
+		body: item	
 	  });
 	  return await response.json();
 	} catch (error) {
 	  return { error: error.message || 'Unknown error' };
-	}
-	
+	}	
   }
+
+
+  async function completeTask(data, apiKey) {
+	const headers = {'Content-Type': 'application/json','Authorization': "Bearer "+ apiKey};
+	const url = 'https://api.todoist.com/rest/v1/tasks/'+data+'/close';
+	try {
+	  await axios.post(url,null,
+		{headers: headers
+	}) 
+	  .then((response) => {
+		return response.data
+	});
+	}	 catch (error) {
+		return { error: error.message || 'Unknown error' };
+	//   return await response.json();
+  }
+}
+
+//   async function completeTask(item, apiKey) {
+// 	try {
+// 	  const response = fetch('https://api.todoist.com/rest/v1/tasks/'+item+'/close', {
+// 		method: 'POST',
+// 		headers: {'Authorization': "Bearer "+ apiKey}
+// 	  });
+// 	  alert(response.data);
+// 	  return true;
+// 	} catch (error) {
+// 	//alert(error.message);
+// 	  return { error: error.message || 'Unknown error' };
+// 	}
+//   }
+
